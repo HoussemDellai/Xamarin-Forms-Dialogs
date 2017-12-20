4118ae57833cfdadbd80f39145ec86a4becf7186
Houssem Dellai <houssem.dellai.ext@geodis.com>
2017-12-20 19:35:38 +0100

    Add project files.

---
+++ Dialogs/Dialogs/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Dialogs
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+	    private async void ShowDisplayAlert(object s, EventArgs e)
+	    {
+	        await DisplayAlert("DisplayAlert", "This an DisplayAlert from Xamarin Forms", "Ok", "Cancel");
+	    }
+
+	    private async void ShowActionSheet(object s, EventArgs e)
+	    {
+	        await DisplayActionSheet("DisplayActionSheet", "Cancel", "Ok", "Copy", "Edit", "Delete");
+	    }
+    }
+}
